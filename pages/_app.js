53161d57094fb1e822c75fb15440efd6ad0932ec
Steven Evans <1st3v3n7@gmail.com>
2018-12-18 00:12:53 -0800

    Add _app.js which runs on top of any page load so the redux store gets initialized

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+// Minimal _app.js setup for redux copied from https://github.com/kirill-konshin/next-redux-wrapper
+
+import React from 'react'
+import {Provider} from "react-redux";
+import App, {Container} from "next/app";
+import withRedux from "next-redux-wrapper";
+import {makeStore} from "../components/store";
+
+export default withRedux(makeStore, {debug: true})(class MyApp extends App {
+
+  static async getInitialProps({Component, ctx}) {
+    return {
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+      }
+    };
+  }
+
+  render() {
+    const {Component, pageProps, store} = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
+});
