ab77be901d207bed6cbfe9a9ecac0e72eb072af0
Steven Evans <1st3v3n7@gmail.com>
2019-01-08 20:06:06 -0800

    Add redux devtools to confirm the store and reducers are doing as expected

--- utils/store.js
+++ utils/store.js
@@ -1,9 +1,10 @@
 import {createStore} from "redux";
 import { fromJS } from "immutable";
 import reducers from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const makeStore = (initialState, options) => {
-  return createStore(reducers, fromJS(initialState));
+  return createStore(reducers, fromJS(initialState), composeWithDevTools());
 };
 
 export default makeStore;
