b05d9ff7bdbcfac766690a2e18241f711e92a069
Steven Evans <1st3v3n7@gmail.com>
2019-03-17 17:12:20 -0700

    Move createEpicMiddleware into the store init function
    Now the epic middleware isn't created multiple times and constantly
    complaining in the console

--- utils/store.js
+++ utils/store.js
@@ -5,9 +5,8 @@ import reducers from "../reducers/root";
 import epics from "../epics/root";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const epicMiddleware = createEpicMiddleware();
-
 const makeStore = (initialState, options) => {
+  const epicMiddleware = createEpicMiddleware();
   const store = createStore(reducers, fromJS(initialState),
     composeWithDevTools(
       applyMiddleware(epicMiddleware)
