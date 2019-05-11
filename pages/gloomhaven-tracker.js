01fb2906c8ef2a47a347dbcfe941ec6a83408e43
Steven Evans <1st3v3n7@gmail.com>
2019-05-11 14:52:34 -0700

    Move initializing SSE to component did mount instead of transition from last page
    This way it will run for those who have there initial page load at the
    tracker and not the setup

--- pages/gloomhaven-tracker.js
+++ pages/gloomhaven-tracker.js
@@ -24,65 +24,80 @@ import {
   createMonster,
   updateNewMonsterDialogue,
   fetchTrackerState,
-  setRoomCode,
+  setRoomCode, initializeSSE,
 } from "../redux/actions/gloomhaven-tracker";
 import rootEpic from "../redux/epics/root";
 import { isBoss } from '../utils/monster';
 
-const GloomhavenTracker = (props) => {
-  return (
-    <React.Fragment>
-      <Grid container spacing={24}>
-        <Grid item xs={12} md={4}>
-          <InitiativeInputCard />
-        </Grid>
-        <Grid container item xs={12} md={8} spacing={24}>
-          {
-            props.initiativeSortedClasses.map((sortedClass) => {
-              let componentToRender;
-              if (!sortedClass[1].get('active')) {
-                componentToRender = <CharacterCard name={sortedClass[0]}/>;
-              } else if (isBoss(sortedClass[1].get('name'))) {
-                //componentToRender = <BossCard name={sortedClass[0]}/>;
-              } else {
-                componentToRender = <MonsterCard name={sortedClass[0]} scenarioLevel={props.scenarioLevel}/>;
-              }
-              return (
-                <Grid key={sortedClass[0]} item xs={12}>
-                  {componentToRender}
-                </Grid>
-              );
-            })
-          }
-        </Grid>
-      </Grid>
-      <NewMonsterDialog
-        open={props.newMonsterDialogueOpen}
-        onSelectStandee={props.onSelectStandee.bind(null, props.newMonsterType, props.scenarioLevel)}
-        numberOfMaxStandees={10}
-        activeStandees={props.activeStandees}
-        onClose={props.onNewMonsterDialogueClose(props.newMonsterType)}
-      />
-    </React.Fragment>
-  );
-};
+class GloomhavenTracker extends React.Component {
 
-GloomhavenTracker.getInitialProps = async function({ query, store, isServer }) {
-  store.dispatch(setRoomCode(query.roomCode));
-  const state$ = new StateObservable(new Subject(), store.getState());
-  const resultAction = await rootEpic(
-    of(fetchTrackerState(query.roomCode)),
-    state$
-  ).toPromise();
-  store.dispatch(resultAction);
+  static async getInitialProps({ query, store, isServer }) {
+    store.dispatch(setRoomCode(query.roomCode));
+    if (isServer) {
+      const state$ = new StateObservable(new Subject(), store.getState());
+      const resultAction = await rootEpic(
+        of(fetchTrackerState(query.roomCode)),
+        state$
+      ).toPromise();
+      store.dispatch(resultAction);
+    }
+    return { isServer, roomCode: query.roomCode }
+  };
 
-  return { isServer }
-};
+  componentDidMount = function() {
+    this.props.onComponentDidMount(this.props.roomCode);
+  };
+
+  render() {
+    const props = this.props;
+
+    return (
+      <React.Fragment>
+        <Grid container spacing={24}>
+          <Grid item xs={12} md={4}>
+            <InitiativeInputCard />
+          </Grid>
+          <Grid container item xs={12} md={8} spacing={24}>
+            {
+              props.initiativeSortedClasses.map((sortedClass) => {
+                let componentToRender;
+                if (!sortedClass[1].get('active')) {
+                  componentToRender = <CharacterCard name={sortedClass[0]}/>;
+                } else if (isBoss(sortedClass[1].get('name'))) {
+                  //componentToRender = <BossCard name={sortedClass[0]}/>;
+                } else {
+                  componentToRender = <MonsterCard name={sortedClass[0]} scenarioLevel={props.scenarioLevel}/>;
+                }
+                return (
+                  <Grid key={sortedClass[0]} item xs={12}>
+                    {componentToRender}
+                  </Grid>
+                );
+              })
+            }
+          </Grid>
+        </Grid>
+        <NewMonsterDialog
+          open={props.newMonsterDialogueOpen}
+          onSelectStandee={props.onSelectStandee.bind(null, props.newMonsterType, props.scenarioLevel)}
+          numberOfMaxStandees={10}
+          activeStandees={props.activeStandees}
+          onClose={props.onNewMonsterDialogueClose(props.newMonsterType)}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+
+
+
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSelectStandee: (monsterType, scenarioLevel, standee, elite) => () => dispatch(createMonster(standee, monsterType, elite, scenarioLevel)),
     onNewMonsterDialogueClose: (monsterType) => () => dispatch(updateNewMonsterDialogue(monsterType, false)),
+    onComponentDidMount: (roomCode) => dispatch(initializeSSE(roomCode)),
   }
 };
 
