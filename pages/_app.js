d6a7b85546ccb17f58556d999ab3b514c01e6317
Steven Evans <1st3v3n7@gmail.com>
2019-05-11 18:13:30 -0700

    Add title and _document to fix css on a server side render
    most of _document is copied from the mui/next example on the mui repo.
    It adds a style tag with css after the serverside render of the app and
    page to the rendered page. When next loads the page for the client, it
    removes that tag.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-// Minimal _app.js setup for redux copied from https://github.com/kirill-konshin/next-redux-wrapper
-
 import React from "react";
 import {Provider} from "react-redux";
 import App, {Container} from "next/app";
+import Head from 'next/head';
 import withRedux from "next-redux-wrapper";
 import makeStore from "../utils/store";
 const {fromJS} = require('immutable');
@@ -18,25 +17,26 @@ class GloomhavenApp extends App {
   }
 
   static async getInitialProps({Component, ctx}) {
-    // we can dispatch from here too
-    //ctx.store.dispatch({type: 'FOO', payload: 'foo'});
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
     return {pageProps};
   }
 
-  /*componentDidMount() {
+  componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-  }*/
+  }
 
   render() {
     const {Component, pageProps, store} = this.props;
     return (
       <Container>
+        <Head>
+          <title>Gloomhaven Monster Tracker</title>
+        </Head>
         <Provider store={store}>
           <JssProvider
             registry={this.pageContext.sheetsRegistry}
