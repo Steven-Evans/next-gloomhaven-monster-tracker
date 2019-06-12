eda5c63f58689e2d75db7522e2ea8732072ca0c2
Steven Evans <1st3v3n7@gmail.com>
2019-06-11 17:29:35 -0700

    Add dotenv and next webpack extension to add environment variables to browser

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'NOTSET';
+export const baseUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : `${process.env.API_HOST}:${process.env.API_PORT}`;
 export const apiUrl = `${baseUrl}/api`;
