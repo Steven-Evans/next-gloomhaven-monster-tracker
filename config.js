43f3bea1bc03d50e61ebb4ef4518d14d5d53a3c2
Steven Evans <1st3v3n7@gmail.com>
2019-08-21 16:41:19 -0700

    Add production environment variable to node process for production start

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : `${process.env.API_HOST}:${process.env.API_PORT}`;
+export const baseUrl = process.env.API_PORT ? `${process.env.API_HOST}:${process.env.API_PORT}` : `${process.env.API_HOST}`;
 export const apiUrl = `${baseUrl}/api`;
